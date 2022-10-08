e29504a48f977415a7904eb915fdf9e5b452d884
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2022-10-08 01:34:43 -0700

    Added new view controller for tweets

--- Twitter/constants.swift
+++ Twitter/constants.swift
@@ -12,8 +12,8 @@ struct K {
     
     struct ApiTokens {
         let authEndPoint = "https://api.twitter.com/oauth/request_token"
-        let apiKey = "ToDrOmtfgheRwHof1EZvJAItx"
-        let apiSecret = "3nhi4s9Bt33XeV3unaDixLAUOopnHcyykc8Fmx0jmi2bO1r4TT"
+        let apiKey = "AGOyDrfsoPcxCf2a9BwFK0jbo"
+        let apiSecret = "KruvxWwl4jeVGnvxh4BCrL4GP5BNRKkP7h6KtwLXGjbwWchswI"
         let accessTooken = "1188222948521308160-DGqZwexlJRV6gmSG63PUWYWTNkRxEF"
         let accessTokenSecret = "QdLlCgzy7nNq4inAgLXljWQWCHoI0En8aQcrcqX2uYySr"
     }
